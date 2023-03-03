5b105830c89658b6ddafff5223e5a6ed06ad6793
its-shreybhatt <shreybhatt2@gmail.com>
2023-03-03 16:37:26 +0530

    Code For Address Book

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -1,5 +1,17 @@
 public class AddressBook {
     public static void main(String[] args) {
         System.out.println(" Welcome to Address Book Program !! ");
+        Contact contact = new Contact();
+
+        contact.name = "Shrey";
+        contact.lastname = "Bhatt";
+        contact.address = "Dehradun Colony";
+        contact.state = "Uttarakhand";
+        contact.zip = 248001;
+        contact.phone = 9876;
+        contact.email = "shrey@123.com";
+
+        System.out.println(contact.name + " " + contact.lastname + " " + contact.address + " "
+                + contact.state + " " + contact.zip + " " + contact.phone + " " + contact.email);
     }
 }
---
+++ src/Contact.java
@@ -0,0 +1,10 @@
+public class Contact {
+    String name;
+    String lastname;
+    String address;
+    String state;
+    int zip;
+    int phone;
+    String email;
+
+}
