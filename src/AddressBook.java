55ac74b71829db593ba7810427e5818e74ac6215
its-shreybhatt <shreybhatt2@gmail.com>
2023-03-03 19:11:07 +0530

    Added : Code to Edit details

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -1,35 +1,122 @@
 import java.util.Scanner;
 
 public class AddressBook {
+    static Scanner input = new Scanner(System.in);
+
     public static void main(String[] args) {
         System.out.println(" Welcome to Address Book Program !! ");
-
-        Scanner input = new Scanner(System.in);
         System.out.println("number of person :");
         int person = input.nextInt();
         Contact[] array = new Contact[person];
-        for (int i = 0; i < person; i++) {
-            System.out.println("enter " + (i + 1) + " person name-");
-            String name = input.next();
-            System.out.println("enter " + (i + 1) + " person lastname-");
-            String lastname = input.next();
-            System.out.println("enter " + (i + 1) + " person address-");
-            String address = input.next();
-            System.out.println("enter " + (i + 1) + " person state-");
-            String state = input.next();
-            System.out.println("enter " + (i + 1) + " person zip code-");
-            int zip = input.nextInt();
-            System.out.println("enter " + (i + 1) + " person contact-");
-            int phone = input.nextInt();
-            System.out.println("enter " + (i + 1) + " person email-");
-            String email = input.next();
-            Contact contact = new Contact(name, lastname, address, state, zip, phone, email);
-            array[i] = contact;
+        int result = 0;
+        while (result != 4) {
+            System.out.println("What do you want to perform");
+            System.out.println("1.ADD , 2.Edit , 3.Print , 4.Stop");
+            result = input.nextInt();
+            switch (result) {
+                case 1:
+                    add(person,array);
+                    break;
+                case 2:
+                    edit(person,array);
+                    break;
+                case 3:
+                    print(person,array);
+                    break;
+                case 4:
+                    stop();
+                    break;
+            }
         }
+    }
+
+    public static void add(int person, Contact[] array) {
 
         for (int i = 0; i < person; i++) {
-            System.out.println(array[i].name + " " + array[i].lastname + " " + array[i].address + " "
-                    + array[i].state + " " + array[i].zip + " " + array[i].phone + " " + array[i].email);
+            Contact contact = new Contact();
+            System.out.println("enter " + (i + 1) + " person name-");
+            contact.setName(input.next());
+            System.out.println("enter " + (i + 1) + " person lastname-");
+            contact.setLastname(input.next());
+            System.out.println("enter " + (i + 1) + " person address-");
+            contact.setAddress(input.next());
+            System.out.println("enter " + (i + 1) + " person state-");
+            contact.setState(input.next());
+            System.out.println("enter " + (i + 1) + " person zip code-");
+            contact.setZip(input.nextInt());
+            System.out.println("enter " + (i + 1) + " person contact-");
+            contact.setPhone(input.nextInt());
+            System.out.println("enter " + (i + 1) + " person email-");
+            contact.setEmail(input.next());
+            array[i] = contact;
         }
+    }
+
+    public static void edit(int person, Contact[] array) {
+        System.out.println("what you want to edit :");
+        System.out.println("name, lastname, address, state, contact");
+        String toEdit = input.next();
+        if (toEdit.equals("name")) {
+            System.out.println("whose name :");
+            String oldName = input.next();
+            for (int j = 0; j < person; j++) {
+                if (oldName.equals(array[j].getName())) {
+                    System.out.println("enter new name :");
+                    String newName=input.next();
+                    array[j].setName(newName);
+                }
+            }
+        }
+        if (toEdit.equals("lastname")) {
+            System.out.println("whose lastname :");
+            String oldLastName = input.next();
+            for (int j = 0; j < person; j++) {
+                if (oldLastName.equals(array[j].getName())) {
+                    System.out.println("enter new Lastname :");
+                    array[j].setLastname(input.next());
+                }
+            }
+        }
+        if (toEdit.equals("address")) {
+            System.out.println("whose address :");
+            String oldaddress = input.next();
+            for (int j = 0; j < person; j++) {
+                if (oldaddress.equals(array[j].getName())) {
+                    System.out.println("enter new address :");
+                    array[j].setAddress(input.next());
+                }
+            }
+        }
+        if (toEdit.equals("state")) {
+            System.out.println("whose state :");
+            String oldstate = input.next();
+            for (int j = 0; j < person; j++) {
+                if (oldstate.equals(array[j].getName())) {
+                    System.out.println("enter new state :");
+                    array[j].setState(input.next());
+                }
+            }
+        }
+        if (toEdit.equals("phone")) {
+            System.out.println("whose phone :");
+            String oldphone = input.next();
+            for (int j = 0; j < person; j++) {
+                if (oldphone.equals(array[j].getName())) {
+                    System.out.println("enter new phone no. :");
+                    array[j].setPhone(input.nextInt());
+                }
+            }
+        }
+    }
+
+    public static void print(int person, Contact[] array) {
+        for (int i = 0; i < person; i++) {
+            System.out.println(array[i].getName() + " " + array[i].getLastname() + " " + array[i].getAddress() + " "
+                    + array[i].getState() + " " + array[i].getZip() + " " + array[i].getPhone() + " " + array[i].getEmail());
+        }
+    }
+
+    public static void stop() {
+        System.out.println("Thank you");
     }
 }
--- src/Contact.java
+++ src/Contact.java
@@ -1,20 +1,65 @@
 public class Contact {
-    String name;
-    String lastname;
-    String address;
-    String state;
-    int zip;
-    int phone;
-    String email;
+    private String name;
+    private String lastname;
+    private String address;
+    private String state;
+    private int zip;
+    private int phone;
+    private String email;
 
-    public Contact(String name, String lastname, String address,
-                   String state, int zip, int phone, String email) {
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
         this.name = name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
         this.lastname = lastname;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
         this.address = address;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
         this.state = state;
+    }
+
+    public int getZip() {
+        return zip;
+    }
+
+    public void setZip(int zip) {
         this.zip = zip;
+    }
+
+    public int getPhone() {
+        return phone;
+    }
+
+    public void setPhone(int phone) {
         this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
         this.email = email;
     }
 }
